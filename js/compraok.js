4b8feca13ad6a8c848a45181fe3e752387829473
Misael <dmisael2000@gmail.com>
2021-10-04 00:56:14 -0300

    cambio de ruta de uso. primero el form, luego el pago y luego la nueva vista de compra exitosa

---
+++ js/compraok.js
@@ -0,0 +1,82 @@
+
+    /*Ejecuta en cada recarga el iFrame*/
+  
+        /*En la segunda recarga ya cambia de pagina*/
+        
+        Swal.fire({
+            title: "¡Compra exitosa!",
+            text:"Nos pondremos en contacto con vos cuando el producto esté despachado.",
+            icon:"success",
+            confirmButtonText:`<a style="color:white; text-decoration:none;"; href="index.html">OK</a> `,
+            
+        }
+       
+        )
+       
+	// text:
+	// html:
+	// icon:
+	// confirmButtonText:
+	// footer:
+	// width:
+	// padding:
+	// background:
+	// grow:
+	// backdrop:
+	// timer:
+	// timerProgressBar:
+	// toast:
+	// position:
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+        // })
+        // setTimeout(() => {
+        //     document.location = "https://xenodochial-curie-170a37.netlify.app/";
+            
+        // }, 2000);
+        
+    
+    
+
+
